04de0e3c97138a07348827861d99eedb3475fcbc
jacob-morrison <jacobm@allenai.org>
2023-07-15 14:30:10 -0700

    print only shapes

--- experiments/casehold_helpers.py
+++ experiments/casehold_helpers.py
@@ -224,16 +224,16 @@ def convert_examples_to_text_to_text(
     outputs = []
 
     for input_ids, attention_mask, label in zip(model_inputs["input_ids"], model_inputs["attention_mask"], model_inputs["labels"]):
-        # print('shapes')
+        print('shapes')
         # print(input_ids)
         # print(attention_mask)
         # print(label)
         input_ids = torch.tensor(input_ids)#.unsqueeze(0)
         attention_mask = torch.tensor(attention_mask)#.unsqueeze(0)
         label = torch.tensor(label)
-        # print(input_ids)
-        # print(attention_mask)
-        # print(label)
+        print(input_ids.shape)
+        print(attention_mask.shape)
+        print(label.shape)
         # input_ids = input_ids.unsqueeze(0)
         # attention_mask = attention_mask.unsqueeze(0)
         outputs.append({
