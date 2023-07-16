2988309c7993a674483b23b53cda2aa62d359673
jacob-morrison <jacobm@allenai.org>
2023-07-16 16:26:49 -0700

    test

--- experiments/case_hold.py
+++ experiments/case_hold.py
@@ -373,8 +373,6 @@ def main():
 		pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
 		label_ids[label_ids == -100] = tokenizer.pad_token_id
 		label_str = tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-		print(pred_str)
-		print(label_str)
 		pred_str = lmap(str.strip, pred_str)
 		label_str = lmap(str.strip, label_str)
 		print(pred_str)
