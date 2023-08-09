1c0dbfafd2dafdbc6219f12ed9d0da2fc45aee45
jacob-morrison <jacobm@allenai.org>
2023-08-08 19:19:27 -0700

    fix

--- experiments/sequence_classification.py
+++ experiments/sequence_classification.py
@@ -14,6 +14,7 @@ import numpy as np
 import random
 
 import transformers
+from transformers.trainer_utils import get_last_checkpoint
 from transformers import (
 	AutoConfig,
 	AutoModelForSequenceClassification,
@@ -403,10 +404,30 @@ def main():
 	trainer.log_metrics("eval", metrics)
 	trainer.save_metrics("eval", metrics)
 
+    # Detecting last checkpoint.
+	last_checkpoint = None
+	if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+		last_checkpoint = get_last_checkpoint(training_args.output_dir)
+		if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+			raise ValueError(
+				f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+				"Use --overwrite_output_dir to overcome."
+			)
+		elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+			logger.info(
+				f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+				"the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+			)
+
 	# Training
 	if training_args.do_train:
+		checkpoint = None
+		if training_args.resume_from_checkpoint is not None:
+			checkpoint = training_args.resume_from_checkpoint
+		elif last_checkpoint is not None:
+			checkpoint = last_checkpoint
 		trainer.train(
-			resume_from_checkpoint=True
+			resume_from_checkpoint=checkpoint
 			# model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
 		)
 		trainer.save_model()
