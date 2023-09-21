aebfdda616d281192c6d407ad2d082353a2ba011
jacob-morrison <jacobm@allenai.org>
2023-09-21 15:47:53 -0700

    flash attn without lora

--- experiments/sequence_classification.py
+++ experiments/sequence_classification.py
@@ -137,9 +137,9 @@ def main():
 			f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
 		)
 	
-	# if model_args.use_flash_attn:
-		# from llama2_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
-		# replace_llama_attn_with_flash_attn()
+	if model_args.use_flash_attn:
+		from llama2_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
+		replace_llama_attn_with_flash_attn()
 
 	# Setup logging
 	logging.basicConfig(
