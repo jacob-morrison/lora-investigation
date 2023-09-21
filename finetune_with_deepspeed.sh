d637ce8c75250bb635d7f27a5d7f28b45789d0bc
jacob-morrison <jacobm@allenai.org>
2023-09-21 14:22:26 -0700

    full length

--- finetune_with_deepspeed.sh
+++ finetune_with_deepspeed.sh
@@ -9,7 +9,7 @@ deepspeed experiments/sequence_classification.py \
     --deepspeed ds_configs/stage3_no_offloading.conf \
     --task_name case-hold --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama_models/7B \
     --use_fast_tokenizer False \
-    --output_dir ../results/ --do_train --do_eval --do_predict --max_seq_length 128 \
+    --output_dir ../results/ --do_train --do_eval --do_predict --max_seq_length 1024 \
     --use_lora False --lora_rank 8 --save_total_limit 1 --load_best_model_at_end \
     --metric_for_best_model accuracy --greater_is_better True --evaluation_strategy steps \
     --eval_steps 10 --save_strategy steps --save_steps 10 \
