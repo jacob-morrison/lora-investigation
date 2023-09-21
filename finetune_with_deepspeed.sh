e4c47789dcb5d45233dde69d004a05f6157d848a
jacob-morrison <jacobm@allenai.org>
2023-09-21 16:41:55 -0700

    t5 xxl lora

--- finetune_with_deepspeed.sh
+++ finetune_with_deepspeed.sh
@@ -13,8 +13,8 @@ accelerate launch \
     --use_deepspeed \
     --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
     experiments/sequence_classification.py \
-    --task_name case-hold --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/13B \
-    --output_dir ../results/ --do_train --do_eval --do_predict --max_seq_length 1024 \
+    --task_name case-hold --model_name_or_path google/t5-xxl-lm-adapt \
+    --output_dir ../results/ --do_train --do_eval --do_predict --max_seq_length 512 \
     --use_lora True --lora_rank 8 --save_total_limit 1 --load_best_model_at_end \
     --metric_for_best_model accuracy --greater_is_better True --evaluation_strategy steps \
     --eval_steps 10 --save_strategy steps --save_steps 10 \
@@ -22,7 +22,6 @@ accelerate launch \
     --per_device_eval_batch_size 8 --seed 1 --gradient_accumulation_steps 1 \
     --max_eval_samples 10 --max_train_samples 10 --max_predict_samples 10 \
     --bf16 True \
-    --use_flash_attn True \
     --dataloader_pin_memory False
 
 # pure deepspeed
