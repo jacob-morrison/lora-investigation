d962d1ad331e7523c0ad6a58aa303dd71b1f92a1
jacob-morrison <jacobm@allenai.org>
2023-09-21 16:26:07 -0700

    test full ft again

--- finetune_with_deepspeed.sh
+++ finetune_with_deepspeed.sh
@@ -5,7 +5,7 @@ TOTAL_BATCH_SIZE=128
 GRADIENT_ACC_STEPS=$(($TOTAL_BATCH_SIZE/$NUM_GPUS/$BATCH_SIZE_PER_GPU))
 echo "Training llama model ${MODEL_SIZE} using $NUM_GPUS GPUs, $BATCH_SIZE_PER_GPU batch size per GPU, $GRADIENT_ACC_STEPS gradient accumulation steps"
 
-# deepspeed \
+# accelerate + deepspeed(?)
 accelerate launch \
     --mixed_precision bf16 \
     --num_machines 1 \
@@ -15,7 +15,7 @@ accelerate launch \
     experiments/sequence_classification.py \
     --task_name case-hold --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B \
     --output_dir ../results/ --do_train --do_eval --do_predict --max_seq_length 1024 \
-    --use_lora True --lora_rank 8 --save_total_limit 1 --load_best_model_at_end \
+    --use_lora False --lora_rank 8 --save_total_limit 1 --load_best_model_at_end \
     --metric_for_best_model accuracy --greater_is_better True --evaluation_strategy steps \
     --eval_steps 10 --save_strategy steps --save_steps 10 \
     --max_steps 50 --learning_rate 1e-4 --per_device_train_batch_size 8 \
@@ -24,6 +24,21 @@ accelerate launch \
     --bf16 True \
     --use_flash_attn True \
     --dataloader_pin_memory False
+
+# pure deepspeed
+# deepspeed experiments/sequence_classification.py \
+#     --deepspeed ds_configs/stage3_no_offloading.conf \
+#     --task_name case-hold --model_name_or_path /net/nfs.cirrascale/allennlp/yizhongw/hf_llama2_models/7B \
+#     --output_dir ../results/ --do_train --do_eval --do_predict --max_seq_length 1024 \
+#     --use_lora True --lora_rank 8 --save_total_limit 1 --load_best_model_at_end \
+#     --metric_for_best_model accuracy --greater_is_better True --evaluation_strategy steps \
+#     --eval_steps 10 --save_strategy steps --save_steps 10 \
+#     --max_steps 50 --learning_rate 1e-4 --per_device_train_batch_size 8 \
+#     --per_device_eval_batch_size 8 --seed 1 --gradient_accumulation_steps 1 \
+#     --max_eval_samples 10 --max_train_samples 10 --max_predict_samples 10 \
+#     --bf16 True \
+#     --use_flash_attn True \
+#     --dataloader_pin_memory False
 
 # /net/nfs.cirrascale/allennlp/yizhongw/hf_llama_models/7B # max length 1024++ # 4 train batch size 8 eval batch size
 #     --use_flash_attn True \
