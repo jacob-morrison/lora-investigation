100999e0eee1b2dd97cad8be78317030965dd2a5
jacob-morrison <jacobm@allenai.org>
2023-10-09 14:43:38 -0700

    update

--- run_deberta_xxlarge.sh
+++ run_deberta_xxlarge.sh
@@ -30,6 +30,23 @@ do
     do
         directory=/net/nfs.cirrascale/allennlp/jacobm/lora-investigation/deberta-v2-xxlarge/case-hold/lora_${RANK}/seed_${SEED}/${LR}/
         mkdir -p ${directory} &&
+        echo "        accelerate launch \
+            --use_deepspeed \
+            --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+            experiments/sequence_classification.py \
+            --task_name sciq \
+            --model_name_or_path microsoft/deberta-v2-xxlarge \
+            --output_dir ${directory} \
+            --seed ${SEED} \
+            --do_train --do_eval --do_predict \
+            --max_seq_length 512 \
+            --use_lora True --lora_rank ${RANK} \
+            --save_total_limit 1 \
+            --load_best_model_at_end --metric_for_best_model accuracy --greater_is_better True \
+            --evaluation_strategy steps --eval_steps 1250 \
+            --save_strategy steps --save_steps 1250 --max_steps 18750 \
+            --learning_rate ${LR} --per_device_train_batch_size 1 --per_device_eval_batch_size 8 \
+            --gradient_accumulation_steps 1 --dataloader_pin_memory False" &&
         accelerate launch \
             --use_deepspeed \
             --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
@@ -46,6 +63,6 @@ do
             --evaluation_strategy steps --eval_steps 1250 \
             --save_strategy steps --save_steps 1250 --max_steps 18750 \
             --learning_rate ${LR} --per_device_train_batch_size 1 --per_device_eval_batch_size 8 \
-            --gradient_accumulation_steps 1 --dataloader_pin_memory False &&
+            --gradient_accumulation_steps 1 --dataloader_pin_memory False
     done
 done
