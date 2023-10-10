7238571013b2fa69dfe80038f51cc88e08144f6d
jacob-morrison <jacobm@allenai.org>
2023-10-09 23:32:11 -0700

    update script

--- run_deberta_xxlarge.sh
+++ run_deberta_xxlarge.sh
@@ -23,52 +23,55 @@
 #     done
 # done
 
-RANK=8
+# RANK=8
 TASK=case-hold
 NUM_GPUS=8
 TRAIN_BATCH_SIZE=8
 EVAL_BATCH_SIZE=32
 MAX_STEPS=$((150000/$NUM_GPUS/$TRAIN_BATCH_SIZE))
 SAVE_STEPS=$((10000/$NUM_GPUS/$TRAIN_BATCH_SIZE))
-for SEED in 1 2 3
+for RANK in 1 2 4 16
 do
-    for LR in 1e-7
+    for SEED in 1 2 3
     do
-        directory=/net/nfs.cirrascale/allennlp/jacobm/lora-investigation/deberta-v2-xxlarge/${TASK}/lora_${RANK}/seed_${SEED}/${LR}/
-        mkdir -p ${directory} &&
-        echo "        accelerate launch \
-            --use_deepspeed \
-            --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
-            experiments/sequence_classification.py \
-            --task_name ${TASK} \
-            --model_name_or_path microsoft/deberta-v2-xxlarge \
-            --output_dir ${directory} \
-            --seed ${SEED} \
-            --do_train --do_eval --do_predict \
-            --max_seq_length 512 \
-            --use_lora True --lora_rank ${RANK} \
-            --save_total_limit 1 \
-            --load_best_model_at_end --metric_for_best_model accuracy --greater_is_better True \
-            --evaluation_strategy steps --eval_steps ${SAVE_STEPS} \
-            --save_strategy steps --save_steps ${SAVE_STEPS} --max_steps ${MAX_STEPS} \
-            --learning_rate ${LR} --per_device_train_batch_size ${TRAIN_BATCH_SIZE} --per_device_eval_batch_size ${EVAL_BATCH_SIZE} \
-            --gradient_accumulation_steps 1 --dataloader_pin_memory False" &&
-        accelerate launch \
-            --use_deepspeed \
-            --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
-            experiments/sequence_classification.py \
-            --task_name ${TASK} \
-            --model_name_or_path microsoft/deberta-v2-xxlarge \
-            --output_dir ${directory} \
-            --seed ${SEED} \
-            --do_train --do_eval --do_predict \
-            --max_seq_length 512 \
-            --use_lora True --lora_rank ${RANK} \
-            --save_total_limit 1 \
-            --load_best_model_at_end --metric_for_best_model accuracy --greater_is_better True \
-            --evaluation_strategy steps --eval_steps ${SAVE_STEPS} \
-            --save_strategy steps --save_steps ${SAVE_STEPS} --max_steps ${MAX_STEPS} \
-            --learning_rate ${LR} --per_device_train_batch_size ${TRAIN_BATCH_SIZE} --per_device_eval_batch_size ${EVAL_BATCH_SIZE} \
-            --gradient_accumulation_steps 1 --dataloader_pin_memory False
+        for LR in 1e-6 5e-7 1e-7
+        do
+            directory=/net/nfs.cirrascale/allennlp/jacobm/lora-investigation/deberta-v2-xxlarge/${TASK}/lora_${RANK}/seed_${SEED}/${LR}/
+            mkdir -p ${directory} &&
+            echo "        accelerate launch \
+                --use_deepspeed \
+                --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+                experiments/sequence_classification.py \
+                --task_name ${TASK} \
+                --model_name_or_path microsoft/deberta-v2-xxlarge \
+                --output_dir ${directory} \
+                --seed ${SEED} \
+                --do_train --do_eval --do_predict \
+                --max_seq_length 512 \
+                --use_lora True --lora_rank ${RANK} \
+                --save_total_limit 1 \
+                --load_best_model_at_end --metric_for_best_model accuracy --greater_is_better True \
+                --evaluation_strategy steps --eval_steps ${SAVE_STEPS} \
+                --save_strategy steps --save_steps ${SAVE_STEPS} --max_steps ${MAX_STEPS} \
+                --learning_rate ${LR} --per_device_train_batch_size ${TRAIN_BATCH_SIZE} --per_device_eval_batch_size ${EVAL_BATCH_SIZE} \
+                --gradient_accumulation_steps 1 --dataloader_pin_memory False" &&
+            accelerate launch \
+                --use_deepspeed \
+                --deepspeed_config_file ds_configs/stage3_no_offloading_accelerate.conf \
+                experiments/sequence_classification.py \
+                --task_name ${TASK} \
+                --model_name_or_path microsoft/deberta-v2-xxlarge \
+                --output_dir ${directory} \
+                --seed ${SEED} \
+                --do_train --do_eval --do_predict \
+                --max_seq_length 512 \
+                --use_lora True --lora_rank ${RANK} \
+                --save_total_limit 1 \
+                --load_best_model_at_end --metric_for_best_model accuracy --greater_is_better True \
+                --evaluation_strategy steps --eval_steps ${SAVE_STEPS} \
+                --save_strategy steps --save_steps ${SAVE_STEPS} --max_steps ${MAX_STEPS} \
+                --learning_rate ${LR} --per_device_train_batch_size ${TRAIN_BATCH_SIZE} --per_device_eval_batch_size ${EVAL_BATCH_SIZE} \
+                --gradient_accumulation_steps 1 --dataloader_pin_memory False
+        done
     done
 done
